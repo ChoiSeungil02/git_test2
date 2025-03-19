14539cb44250421089130e694894da45b28abcb1
Choi, Seungil <jschoi020102@gmail.com>
2025-03-19 10:59:09 +0900

    Hello 클래스 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -2,8 +2,13 @@ package git_test2;
 
 public class Hello {
 
+	String message; public Hello() {
+		this("안녕하세요");
+		} 
+	public Hello(String message) {
+		this.message = message; 
+		} 
 	public void say() {
-		System.out.println("안녕하세요");
-		// TODO Auto-generated method stub
-	}
+		System.out.println(message);
+		}
 }
